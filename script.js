f5d5cbd4bab00ea92f6b2adfa1d14db7317cc725
Tomas Olocco <tomasolocco04@gmail.com>
2023-06-14 15:59:53 -0300

    Commit de Vue

--- script.js
+++ script.js
@@ -1,58 +1,50 @@
-const appGlobo = Vue.crearApp({
+const appGlobo = Vue.createApp({
   data (){
-    return {
-      usuario: 'Jane Doe'
+    return {      
+      boton: 'Seguir',
+      siguiendo: false,
+      numero: 200,
+      megusta: true,
+      usuario: '',
+      error: '',
+      mensaje: '',
+      comentarios: []
+    }
+  },
+  methods: {
+    seguir() {
+      if (this.siguiendo) {
+        this.boton = "Seguir";
+      } else {
+        this.boton = "Dejar de seguir";
+      }
+      this.siguiendo = !this.siguiendo;
+    },
+    like() {
+      if(this.megusta) {
+        this.numero++;
+        this.megusta = false;
+      }
+    },
+    agregarComentario() {
+      if(this.usuario.trim() === '') {
+        this.error = "Ingrese un usuario";
+      }
+      else {
+        if(this.mensaje.trim() === '') {
+          this.error = "Ingrese un comentario";
+        }
+        else {
+          this.comentarios.push(this.usuario + ' ' + this.mensaje);
+          this.mensaje='';
+          this.usuario='';
+        }
+      }
+    },
+    eliminarComentario(index) {
+      this.comentarios.splice(index, 1);
     }
   }
 })
-
-function MSeguir() {
-    var btn = document.getElementById("btnSeguir");
-    if (btn.innerHTML == "Seguir") {
-      btn.innerHTML = "Dejar de Seguir";
-      btn.classList.add("Siguiendo");
-    } else {
-      btn.innerHTML = "Seguir";
-      btn.classList.remove("Siguiendo");
-    }
-  }
-  var Usuario = "";
-  var Comentarios = []
-
-  function IngresarNombre(event){
-    event.preventDefault();
-    Usuario = document.getElementById("IngresarUsuario").value;
-    document.getElementById("IngresarUsuario").value = "";
-  }
-  function AgregarComentarios(event){
-    event.preventDefault();
-  var ComentarioContenido = document.getElementById("Comentarios").value;
-  var Comentario = {
-    usuario: Usuario,
-    contenido: ComentarioContenido
-  };
-  Comentarios.push(Comentario);
-  MostrarComentarios();
-  document.getElementById("Comentarios").value = "";
-  }
-function MostrarComentarios(){  
-var CajaComentarios = document.getElementById("SeccionComentarios");
-  CajaComentarios.innerHTML = "";
-  for (var i = 0; i < Comentarios.length; i++) {
-    var Comentario = Comentarios[i];
-    var NuevoComentario = document.createElement("div");
-    NuevoComentario.innerHTML = "<strong>" + Comentario.usuario + "</strong>: " + Comentario.contenido;
-    CajaComentarios.appendChild(NuevoComentario);
-  }
-}
-let Contadormg = 200;
-let boton = document.getElementById("btnMegusta");
-let MeGustaPresionado = true
-
-function Megusta(){
-  if (MeGustaPresionado){
-    Contadormg++;
-    document.getElementById("Contadormg").innerHTML = Contadormg;
-    MeGustaPresionado = false;
-  }
-}
+appGlobo.mount('#app');
+  
